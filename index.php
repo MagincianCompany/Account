7dc1f1bd5785c0971ce12fbeb1d3cdf09ccce431
MagincianCompany <77320870+MagincianCompany@users.noreply.github.com>
2021-10-04 17:28:45 +0200

    0

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
-echo banana;
+    echo "get the banana";
 
-?>
-<a href="https://www.heroku.com"> Heroku.com</a>
+    ?>
+</body>
+</html>
